05d1ecea4fc952721da033db27012b411ec18aa9
Pedro Javier González Mijarra <pedro96javier@gmail.com>
2025-07-02 23:18:10 +0200

    Cambios tags en router genérico y cambiados statusCodes por constantes

--- src/routers/movie_router.py
+++ src/routers/movie_router.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from fastapi import HTTPException, Path, Query, APIRouter
+from fastapi import HTTPException, Path, Query, APIRouter, status
 from src.models.movie_model import Movie, Movie_create, Movie_update
 
-movie_router = APIRouter()
+movie_router = APIRouter(prefix='/movies', tags=['Movies'])
 
 movies: List[Movie] = [
     Movie(
@@ -26,7 +26,7 @@ movies: List[Movie] = [
 ## CRUD ##
 
 # CREATE
-@movie_router.post("/", status_code=201, response_model= List[Movie], tags=['Movies'])
+@movie_router.post("/", status_code=201, response_model= List[Movie])
 async def create_movie(movie: Movie_create) -> List[Movie]:
     new_movie = Movie(**movie.model_dump())
     movies.append(new_movie)
@@ -34,7 +34,7 @@ async def create_movie(movie: Movie_create) -> List[Movie]:
     return movies
 
 # UPDATE
-@movie_router.put("/{id}", response_model=Movie, tags=['Movies'])
+@movie_router.put("/{id}", response_model=Movie)
 async def update_movie(id:int, movie: Movie_update) -> Movie:
     for item in movies:
         if item.id == id:
@@ -46,10 +46,10 @@ async def update_movie(id:int, movie: Movie_update) -> Movie:
 
         return item
     
-    raise HTTPException(status_code=404, detail="Movie not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
 
 # DELETE
-@movie_router.delete("/{id}", response_model= List[Movie],  tags=['Movies'])
+@movie_router.delete("/{id}", response_model= List[Movie])
 async def delete_movie(id: int) -> List[Movie]:
     for movie in movies:
         if movie.id == id:
@@ -57,32 +57,32 @@ async def delete_movie(id: int) -> List[Movie]:
 
         return movies
     
-    raise HTTPException(status_code=404, detail="Movie not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
 
 ## SEARCH ##
 
 # ALL MOVIES
-@movie_router.get("/", response_model= List[Movie], tags=['Movies'])
+@movie_router.get("/", response_model= List[Movie])
 async def get_movies() -> List[Movie]:
 
     return movies
 
 # MOVIE BY CATEGORY
-@movie_router.get("/category", response_model= List[Movie], tags=['Movies'])
+@movie_router.get("/category", response_model= List[Movie])
 async def get_movie_by_category(category: str = Query(min_length=5, max_length=20)) -> List[Movie]:
     result = [movie for movie in movies if movie.category == category]
     if not result:
 
-        raise HTTPException(status_code=404, detail="No movies found in that category")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No movies found in that category")
     
     return result
 
 # MOVIE BY ID
-@movie_router.get("/{id}", response_model=Movie, tags=['Movies'])
+@movie_router.get("/{id}", response_model=Movie)
 async def get_movie(id: int = Path(gt=0)) -> Movie:
     for movie in movies:
         if movie.id == id:
 
             return movie
         
-    raise HTTPException(status_code=404, detail="Movie not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
